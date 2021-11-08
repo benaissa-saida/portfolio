5e357469c5c9d54783fb6786ce50813e54e4d9b4
benaissa-saida <benaissasaida28@gmail.com>
2021-11-08 12:12:23 +0100

    images for readme & vue.config edit

--- vue.config.js
+++ vue.config.js
@@ -36,26 +36,7 @@ module.exports = {
   // //     new webpack.DefinePlugin({
   // //       'process.env.NODE_ENV': JSON.stringify('production')
   // //     })
-  // //   ],    
-  //   optimization: {
-  //     nodeEnv: 'production',
-  //     minimize: true,
-  //     runtimeChunk: 'single',
-  //     splitChunks: {
-  //       chunks: 'all',
-  //       maxInitialRequests: Infinity,
-  //       minSize: 0,
-  //       cacheGroups: {
-  //         vendor: {
-  //           test: /[\\/]node_modules[\\/]/,
-  //           name(module) {
-  //             const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
-  //             return `npm.${packageName.replace('@', '')}`;
-  //           },
-  //         },
-  //       },
-  //     },
-  //   },
+  // //   ], 
   // },
     
 };
